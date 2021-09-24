5b8a6edd4553e5d38e2b77e62f3a9bb3668eded5
01000001-01000111 <48495702+01000001-01000111@users.noreply.github.com>
2021-09-24 14:31:56 -0400

    Update SCCM-Remote-Viewer-CmRcViewer-on-Mulitple-Computers.ps1

--- SCCM-Remote-Viewer-CmRcViewer-on-Mulitple-Computers.ps1
+++ SCCM-Remote-Viewer-CmRcViewer-on-Mulitple-Computers.ps1
@@ -1,6 +1,3 @@
-
-
-
 <# This connects SCCM Remote Viewer to multiple computers listed in C:\temp\computers.txt
 Create and enter the computer names or IP address's in C:\temp\computers.txt
 Created by Aric Galloso, 9/24/2021
